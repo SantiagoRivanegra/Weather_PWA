c237939ddb988fbb19e6dd32fb7672464aefdc32
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-07-19 21:52:31 -0300

    Add lang, css theme and validate empty search

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ const App = () => {
 
   const search = async (e) => {
     if(e.key === 'Enter') {
-      const data = await fetchWeather(query)
       
+      if(!query){
+        lang === 'es' ? alert('Ingrese una ciudad') : alert('Enter a city')
+      }
+      
+      const data = await fetchWeather(query, {lang})      
       setWeather(data)
       setQuery('')
     }
@@ -24,12 +28,19 @@ const App = () => {
     light === "OFF" ? setLight("ON") : setLight("OFF"); 
   }
 
+  const [lang, setLang] = useState("es")
+  function handleLang(e){
+    e.preventDefault(e);
+    lang === "es" ? setLang("en") : setLang("es"); 
+  }
+
   return (
     <div className="container">
-      <NavBar light={light} handleLight={handleLight}/>
+      <NavBar light={light} handleLight={handleLight} lang={lang} handleLang={handleLang}/>
     <div className="main-container">
       {/* {console.log(light)} */}
-      <input type='text' className='search' placeholder='Search...' value={query} id={light} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+      {console.log(lang)}
+      <input type='text' className='search' placeholder={lang==='es' ? "Buscar..." : "Search..."} value={query} id={light} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
       {weather.main && (
         <div className="city" id = {light}>
           <h2 className="city-name" id = {light}>
